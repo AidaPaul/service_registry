c6554ab9437ab41573f304a08eb5a6709e54967d
Laszlowaty <laszla.niet@gmail.com>
2016-03-12 22:06:20 +0100

    I surrender

--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-from service import Service
+from service import Service, ServiceState
 from exceptions import NoServiceWithId
 
 def passed(function):
@@ -14,6 +14,22 @@ def constructor_test():
 	else:
 		n_passed(constructor_test)
 
+def update_service_name():
+	myService = Service("test1", 0.1)
+	Service.update_service(myService, new_service_name = "test2") ## updating name
+	if myService.service_name == "test2" and myService.current_version == 0.1 and myService.state == ServiceState.changed:
+		passed(update_service_name)
+	else:
+		n_passed(update_service_name)
+
+def update_service_version():
+	myService = Service("test1", 0.1)
+	Service.update_service(myService, new_service_version = 0.2) ## updating name
+	if myService.service_name == "test1" and myService.current_version == 0.2 and myService.state == ServiceState.changed:
+		passed(update_service_name)
+	else:
+		n_passed(update_service_name)
+
 def exception_test():
 	try:
 		Service.get_service_by_id(40)
@@ -24,3 +40,5 @@ def exception_test():
 
 constructor_test()
 exception_test()
+update_service_name()
+update_service_version()
