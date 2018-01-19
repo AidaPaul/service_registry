3356e63f077a520a4c1480d39b52ce9dc410993b
Jadav Bheda <jadav.bheda@gmail.com>
2018-01-19 14:41:02 +1100

    search filter added: /services/?search=test

--- service_registry_rest_app/service_registry/views.py
+++ service_registry_rest_app/service_registry/views.py
@@ -3,11 +3,15 @@ from __future__ import unicode_literals
 from rest_framework import viewsets
 from serializers import ServiceSerializer
 from models import Service
+from rest_framework import filters
 
 
 class ServiceViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows users to be viewed or edited.
+    API endpoint that allows Services to be viewed or edited.
     """
     queryset = Service.objects.all()
     serializer_class = ServiceSerializer
+
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('=name',)
