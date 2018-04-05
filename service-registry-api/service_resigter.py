70d77276e45decce6fd77596636ce4ea015957c1
Ali <ali.radd@gmail.com>
2018-04-05 06:29:41 +0100

    added Finding service without version

--- service-registry-api/service_resigter.py
+++ service-registry-api/service_resigter.py
@@ -33,13 +33,20 @@ def get_service_id(service_id):
         abort(404)
     return jsonify({'service': service[0]})
 	
-@app.route('/service_registry/api/v1.0/findServices/<service_name>', methods=['GET'])
-def get_service(service_name):
-    service = [service for service in services if service['service'] == service_name]
+@app.route('/service_registry/api/v1.0/findServices/<service_name_all>/getAll', methods=['GET'])
+def get_service_all(service_name_all):
+    service = [service for service in services if service['service'] == service_name_all]
     if len(service) == 0:
         abort(404)
     return jsonify({'service': service})
 	
+@app.route('/service_registry/api/v1.0/findServices/<service_name>', methods=['GET'])
+def get_service(service_name):
+    service = [service for service in services if service['service'] == service_name]
+    if len(service) == 0:
+            return jsonify({'service': service_name,'count': 0 })
+    count=len(service)	
+    return jsonify({'service': service_name,'count': count })
 @app.route('/service_registry/api/v1.0/findServices/<service_name>/<service_version>', methods=['GET'])
 def get_service_count(service_name,service_version):
     service = [service for service in services if service['service'] == service_name and service['version'] == service_version]
