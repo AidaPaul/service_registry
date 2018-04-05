1b21d891f853b936750086c002933051d6e21515
Ali Rad Mac <ali@butlerthing.io>
2018-04-05 08:41:39 +0100

    ran through behave, fixed feature files and created snipets for test automation

---
+++ features/steps/service_register_snippents.py
@@ -0,0 +1,278 @@
+@when(u'I add a service "test" with version "0.0.1"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I add a service "test" with version "0.0.1"')
+
+
+@then(u'I should be notified with a change "created and a unique service identifier 0.0.1.1.epochtime"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then I should be notified with a change "created and a unique service identifier 0.0.1.1.epochtime"')
+
+
+@then(u'I should be notified with a change "created and a unique service identifier 0.0.1.2.epochtime"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then I should be notified with a change "created and a unique service identifier 0.0.1.2.epochtime"')
+
+
+@when(u'I add a service "test" with version "0.0.2"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I add a service "test" with version "0.0.2"')
+
+
+@then(u'I should be notified with a change "created and a unique service identifier 0.0.2.1.epochtime"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then I should be notified with a change "created and a unique service identifier 0.0.2.1.epochtime"')
+
+
+@then(u'I should be notified with a change "created and a unique service identifier 0.0.2.2.epochtime"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then I should be notified with a change "created and a unique service identifier 0.0.2.2.epochtime"')
+
+
+@when(u'I add a service "test2" with version "0.0.2"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I add a service "test2" with version "0.0.2"')
+
+
+@given(u'add_Service_Registry is run')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given add_Service_Registry is run')
+
+
+@given(u'the following services exist')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given the following services exist')
+
+
+@given(u'I call to remove a test')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Given I call to remove a test')
+
+
+@when(u'I remove the service')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I remove the service')
+
+
+@then(u'the service should be removed')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then the service should be removed')
+
+
+@then(u'I should be notified with a change "removed"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then I should be notified with a change "removed"')
+
+
+@when(u'I search for a service "test"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I search for a service "test"')
+
+
+@then(u'I should get a list of all the services for test')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then I should get a list of all the services for test')
+
+
+@then(u'the response should be paged')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then the response should be paged')
+
+
+@when(u'I search for a service "test2"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I search for a service "test2"')
+
+
+@then(u'I should get a list of all the services for test2')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then I should get a list of all the services for test2')
+
+
+@when(u'I search for a service/show_all "test" with version "0.0.1"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I search for a service/show_all "test" with version "0.0.1"')
+
+
+@then(u'I should find count "2" instances of service')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then I should find count "2" instances of service')
+
+
+@then(u'the service "test" should have the correct type')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then the service "test" should have the correct type')
+
+
+@then(u'the service "test" should have the correct version "0.0.1"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then the service "test" should have the correct version "0.0.1"')
+
+
+@then(u'I should find count "2" services')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then I should find count "2" services')
+
+
+@when(u'I search for a service/show_all "test" with version "0.0.2"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I search for a service/show_all "test" with version "0.0.2"')
+
+
+@then(u'the service "test" should have the correct version "0.0.2"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then the service "test" should have the correct version "0.0.2"')
+
+
+@when(u'I search for a service/show_all "test2" with version "0.0.2"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I search for a service/show_all "test2" with version "0.0.2"')
+
+
+@then(u'the service "test2" should have the correct type')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then the service "test2" should have the correct type')
+
+
+@then(u'the service "test2" should have the correct version "0.0.2"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then the service "test2" should have the correct version "0.0.2"')
+
+
+@when(u'I search for a service "test" with version "0.0.4"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I search for a service "test" with version "0.0.4"')
+
+
+@then(u'I should find count "0" services')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then I should find count "0" services')
+
+
+@when(u'I search for a service "test3" with version "0.0.1"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I search for a service "test3" with version "0.0.1"')
+
+
+@when(u'I search for a service "test" without version')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I search for a service "test" without version')
+
+
+@then(u'I should find count "4" services')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then I should find count "4" services')
+
+
+@when(u'I search for a service "test2" without version')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I search for a service "test2" without version')
+
+
+@when(u'I search for a service "test" with version "0.0.1" with number "1"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I search for a service "test" with version "0.0.1" with number "1"')
+
+
+@then(u'I should get the "0.0.1.1.epochtime" of service')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then I should get the "0.0.1.1.epochtime" of service')
+
+
+@when(u'I search for a service "test" with version "0.0.1" with number "2"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I search for a service "test" with version "0.0.1" with number "2"')
+
+
+@then(u'I should get the "0.0.1.2.epochtime" of service')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then I should get the "0.0.1.2.epochtime" of service')
+
+
+@when(u'I search for a service "test" with version "0.0.2" with number "1"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I search for a service "test" with version "0.0.2" with number "1"')
+
+
+@then(u'I should get the "0.0.2.1.epochtime" of service')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then I should get the "0.0.2.1.epochtime" of service')
+
+
+@when(u'I search for a service "test" with version "0.0.2" with number "2"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I search for a service "test" with version "0.0.2" with number "2"')
+
+
+@then(u'I should get the "0.0.2.2.epochtime" of service')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then I should get the "0.0.2.2.epochtime" of service')
+
+
+@when(u'I search for a service "test2" with version "0.0.2" with number "1"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I search for a service "test2" with version "0.0.2" with number "1"')
+
+
+@when(u'I search for a service "test2" with version "0.0.2" with number "2"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I search for a service "test2" with version "0.0.2" with number "2"')
+
+
+@when(u'I search for a service "test" with version "0.0.1" with number "3"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I search for a service "test" with version "0.0.1" with number "3"')
+
+
+@when(u'I search for a service "test" with version "0.0.1" with number "b"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I search for a service "test" with version "0.0.1" with number "b"')
+
+
+@when(u'I search for a service "test" with version "0.0.2" with number "3"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I search for a service "test" with version "0.0.2" with number "3"')
+
+
+@when(u'I search for a service "test" with version "0.0.2" with number "10"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I search for a service "test" with version "0.0.2" with number "10"')
+
+
+@when(u'I search for a service "test4" with version "0.0.2" with number "1"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I search for a service "test4" with version "0.0.2" with number "1"')
+
+
+@when(u'I search for a service "test" with version "0.0.4" with number "1"')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I search for a service "test" with version "0.0.4" with number "1"')
+
+
+@when(u'I update a test2')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I update a test2')
+
+
+@then(u'I should be notified with a change "changed" and 4 should increment')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then I should be notified with a change "changed" and 4 should increment')
+
+
+@then(u'update will happen to all services named test2')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then update will happen to all services named test2')
+
+
+@when(u'I update a test and 0.0.2')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I update a test and 0.0.2')
+
+
+@then(u'I should be notified with a change "changed" and 3 should increment to one higer than biggest 3 for this service and version combinaiton')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then I should be notified with a change "changed" and 3 should increment to one higer than biggest 3 for this service and version combinaiton')
+
+
+@then(u'update will happen to all services named test with version 0.0.2 only')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then update will happen to all services named test with version 0.0.2 only')
