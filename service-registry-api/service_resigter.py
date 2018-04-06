38a017bf702f843fbc541430c1b4cf8b0bed22e8
Ali <ali.radd@gmail.com>
2018-04-06 02:40:01 +0100

    added comments to the code

--- service-registry-api/service_resigter.py
+++ service-registry-api/service_resigter.py
@@ -10,7 +10,8 @@ from flask import url_for
 app = Flask(__name__)
 
 
-	  
+#We use a temporary in memory json object to store the services
+#This of course should be on a DB in the long term.	  
 services = [
     {
         'id': 1,
@@ -56,18 +57,21 @@ services = [
     }
 ]
 
+#This is our get function at the high level. This will return a json object including all registered services.
+#We also return a URI refering to each service so that the users don't have to create that
 @app.route('/service_registry/api/v1.0/services', methods=['GET'])
 def get_services():
 #    return jsonify({'services': services})
      return jsonify({'services': [make_public_service(service) for service in services]})
 
+#This get function returns a service by service id
 @app.route('/service_registry/api/v1.0/services/<int:service_id>', methods=['GET'])
 def get_service_id(service_id):
     service = [service for service in services if service['id'] == service_id]
     if len(service) == 0:
         abort(404)
     return jsonify({'service': service[0]})
-	
+#This end point returns all instances and versions for a given service
 @app.route('/service_registry/api/v1.0/services/<service_name_all>/getAll', methods=['GET'])
 def get_service_all(service_name_all):
     service = [service for service in services if service['service'] == service_name_all]
@@ -78,7 +82,7 @@ def get_service_all(service_name_all):
 
 
 
-
+#This returns the count of a given service
 @app.route('/service_registry/api/v1.0/services/<service_name>', methods=['GET'])
 def get_service(service_name):
     service = [service for service in services if service['service'] == service_name]
@@ -86,6 +90,7 @@ def get_service(service_name):
             return jsonify({'service': service_name,'count': 0 })
     count=len(service)	
     return jsonify({'service': service_name,'count': count })
+#this returns the counts of a service and version
 @app.route('/service_registry/api/v1.0/services/<service_name>/<service_version>', methods=['GET'])
 def get_service_count(service_name,service_version):
     service = [service for service in services if service['service'] == service_name and service['version'] == service_version]
@@ -100,8 +105,7 @@ def get_service_count(service_name,service_version):
 @app.errorhandler(404)
 def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
-
-#@app.route('/service_registry/api/v1.0/services/<service_name>/<service_version>', methods=['POST'])
+#This is the post end point implementation
 @app.route('/service_registry/api/v1.0/services', methods=['POST'])
 def create_service():
     if not request.json or not 'service' in request.json or not 'version' in request.json:
@@ -116,7 +120,8 @@ def create_service():
 
     
 
-
+#This is the update service using the service id
+#Other types of update needs to be implemented
 @app.route('/service_registry/api/v1.0/services/<int:service_id>', methods=['PUT'])
 def update_service(service_id):
     service = [service for service in services if service['id'] == service_id]
@@ -133,9 +138,9 @@ def update_service(service_id):
     service[0]['version'] = request.json.get('service', service[0]['service'])
     service[0]['version'] = request.json.get('version', service[0]['version'])
     service[0]['change'] = 'changed'
-#    service[0]['change_version'] = request.json.get('done', service[0]['done']))
     return jsonify({'service': service[0]})
 
+#This is the delete end point
 @app.route('/service_registry/api/v1.0/services/<int:service_id>', methods=['DELETE'])
 def delete_service(service_id):
     service = [service for service in services if service['id'] == service_id]
@@ -145,7 +150,7 @@ def delete_service(service_id):
     return jsonify({'change': 'removed'})
     services.append(service)
     return jsonify({'service': service}), 201
-
+#This function is used to create a URI to our services. This helps the user interact with the service
 def make_public_service(service):
     new_service = {}
     for field in service:
