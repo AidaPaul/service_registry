2db69f84bf6fc2f26893f90244ee06b4a780e425
Ali <ali.radd@gmail.com>
2018-04-07 17:52:44 +0100

    added sceleton for put /services/service_name this endpoint is not implemented yet

--- service-registry-api/service_resigter.py
+++ service-registry-api/service_resigter.py
@@ -152,6 +152,30 @@ def update_service(service_id):
     service[0]['change_version']=service[0]['change_version']+1
     return jsonify({'service': service[0]})
 
+#This is the put implementation for /services/service_name
+#We have not implemented this yet
+@app.route('/service_registry/api/v1.0/services/<service_name>', methods=['PUT'])
+def update_service_serviceName(service_name):
+    service = [service for service in services if service['service'] == service_name]
+    if len(service) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'service' in request.json and type(request.json['service']) != str:
+        abort(400)
+    if 'version' in request.json and type(request.json['version']) != str:
+        abort(400)
+    if 'change' in request.json and type(request.json['change']) != str:
+        abort(400)
+    return jsonify({'Endpoint': 'Not implmented'})
+#    service[0]['service'] = request.json.get('service', service[0]['service'])
+#    service[0]['version'] = request.json.get('version', service[0]['version'])
+#    service[0]['change'] = u'changed'
+#    service[0]['change_version']=service[0]['change_version']+1
+    return jsonify({'service': service[0]})
+
+
+
 #This is the delete end point
 @app.route('/service_registry/api/v1.0/services/<int:service_id>', methods=['DELETE'])
 def delete_service(service_id):
