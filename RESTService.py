be9eb9c6f7ad8f60cdc77d29f79ff936c903cc35
duch <top@secret.org>
2018-05-29 22:09:10 +0200

    RESTService constructor and start method definitions

--- RESTService.py
+++ RESTService.py
@@ -5,4 +5,12 @@ from flask_restful import Resource, Api, reqparse
 
 class RESTService(Resource):
     def __init__(self):
-        pass
+        self.registry = Registry()
+        self.parser = reqparse.RequestParser()
+
+    @staticmethod
+    def start():
+        app = Flask(__name__)
+        api = Api(app)
+        api.add_resource(RESTService, '/api/services')
+        app.run(port='5002')
