5e2248e8da535944971d2025eef9fd89c8e27d6e
duch <top@secret.org>
2018-05-29 22:11:07 +0200

    local InputValidator class for RESTService

--- RESTService.py
+++ RESTService.py
@@ -15,6 +15,20 @@ class RESTService(Resource):
         api.add_resource(RESTService, '/api/services')
         app.run(port='5002')
 
+    class InputValidator(object):
+        @staticmethod
+        def not_empty_str(s):
+            if s == '':
+                raise reqparse.exceptions.BadRequest("Param can not be empty")
+            return s
+
+        @staticmethod
+        def positive_int(i):
+            i = int(i)
+            if i < 1:
+                raise reqparse.exceptions.BadRequest("Param can not be empty")
+            return i
+
     @staticmethod
     def reply(result):
         return {"result": result}
