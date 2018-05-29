254401b2a64d3a2b6711ca7a678d4dde72aff4fd
duch <top@secret.org>
2018-05-29 21:55:52 +0200

    importing sqlite3 and setting it up for Registry class

--- Registry.py
+++ Registry.py
@@ -1,3 +1,9 @@
+import sqlite3
+
+
 class Registry(object):
     def __init__(self):
-        pass
+        self.conn = sqlite3.connect('service_registry.db')
+        self.conn.isolation_level = None
+        self.conn.row_factory = sqlite3.Row
+
